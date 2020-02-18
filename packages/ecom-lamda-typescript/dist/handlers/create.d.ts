93c3007e6098c8654cf1f209cf3093aa01293fbe
Alex Brown <alex.brown@photobox.com>
2020-02-18 16:17:35 +0000

    Updated readme and documentation

--- packages/ecom-lamda-typescript/dist/handlers/create.d.ts
+++
@@ -1,8 +0,0 @@
-import { Context } from 'aws-lambda';
-/**
- *
- * @param event
- * @param context
- * @param callback
- */
-export declare const createSingular: (event: import("aws-lambda").APIGatewayProxyEvent, context: Context, callback: import("aws-lambda").Callback<import("aws-lambda").APIGatewayProxyResult>) => void;
